f762d332245b415d915e5e6ad302729b5c791d8d
Theerada Sirisumthum (Milk) <theerada.sirisumthum@anca.com>
2022-05-26 08:22:54 +0700

    Add header to QR image

--- app.py
+++ app.py
@@ -10,6 +10,20 @@ from PIL import Image, ImageDraw, ImageFont
 # fonts_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fonts')
 # font = ImageFont.truetype(os.path.join(fonts_path, 'sans_serif.ttf'), 24)
 l1 = []
+
+# Create header image
+def header(output_path):
+    image = Image.new("RGB", (370,50), "white")
+    draw = ImageDraw.Draw(image)
+    color = "red"
+    x = 20
+    y = 10
+    text = 'Connect to ANCA CNC WIFI'
+    font = ImageFont.truetype(r'filepath\..\Alef-Bold.ttf', size = 25)
+    draw.text((x, y), text, font = font, fill = color)
+    image.save(output_path)
+    return image.size
+
 def read_csv_to_list(filename,l1):
     with open(filename, 'r', newline ='') as file:
         reader = csv.reader(file, delimiter = '\n')
@@ -47,7 +61,54 @@ def draw(filename,i):
     # optional parameters like optimize and quality
     # image.save('optimized.png', optimize=True, quality=50)
     
-    
+# Combine images
+def append_images(images, direction='horizontal', bg_color=(255,255,255), aligment='center', widths=list(), heights=list()):
+
+    """
+    Appends images in horizontal/vertical direction.
+
+    Args:
+        images: List of PIL images
+        direction: direction of concatenation, 'horizontal' or 'vertical'
+        bg_color: Background color (default: white)
+        aligment: alignment mode if images need padding;
+           'left', 'right', 'top', 'bottom', or 'center'
+
+    Returns:
+        Concatenated image as a new PIL image object.
+    """
+
+    if direction=='horizontal':
+        new_width = sum(widths)
+        new_height = max(heights)
+    else:
+        new_width = max(widths)
+        new_height = sum(heights)
+
+    new_im = Image.new('RGB', (new_width, new_height), color=bg_color)
+
+
+    offset = 0
+    for im in images:
+        if direction=='horizontal':
+            y = 0
+            if aligment == 'center':
+                y = int((new_height - im.size[1])/2)
+            elif aligment == 'bottom':
+                y = new_height - im.size[1]
+            new_im.paste(im, (offset, y))
+            offset += im.size[0]
+        else:
+            x = 0
+            if aligment == 'center':
+                x = int((new_width - im.size[0])/2)
+            elif aligment == 'right':
+                x = new_width - im.size[0]
+            new_im.paste(im, (x, offset))
+            offset += im.size[1]
+
+    return new_im
+
 def main():    
     read_csv_to_list('library.csv',l1)    
     
@@ -58,15 +119,37 @@ def main():
         border=4,
     )
 
+    widths = []
+    heights = []
+
+    # Draw header
+    (width_head, height_head) = header(r'.\output\header.png')
+
+    widths.append(width_head)
+    heights.append(height_head)
+
     for i in l1:
         qr.add_data(f'https://amtte.onrender.com/lib/{i}')
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
         img = qrcode.make(f'https://amtte.onrender.com/lib/{i}')
         type(img)  # qrcode.image.pil.PilImage
+
+        # Add size of QR in list of wigths and heights
+        if i == l1[0]:
+            width_qr = img.size[0]
+            height_qr = img.size[1]
+            widths.append(width_qr)
+            heights.append(height_qr)
+
         img.save(r'.\output\{}.png'.format(i))
         draw(r'.\output\{}.png'.format(i),i)
 
-        
+        # Combine header image and qr image
+        combine_img = map(Image.open, [r'.\output\header.png', r'.\output\{}.png'.format(i)])
+        combine = append_images(combine_img, direction='vertical', bg_color=(0, 0, 0), aligment='center', widths=widths, heights=heights)
+        combine.save(r'.\output\{}.png'.format(i))
+
+
 if __name__ == "__main__":
     main()
